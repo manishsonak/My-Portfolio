86110ad157e63c6d1d67fe2e1e50433410a848a1
manishsonak <manishsonak9@gmail.com>
2024-08-06 16:34:10 +0530

    Page Not Found

--- src/Components/PageNotFound/PageNotFound.jsx
+++ src/Components/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,11 @@
-
+import { Link } from 'react-router-dom';
 function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <div className="page-not-found">
+      <h1 className="title">404</h1>
+      <p className="message">Oops! The page you’re looking for doesn’t exist.</p>
+      <Link to="/" className="home-link">Go Back Home</Link>
+    </div>
   )
 }
 
